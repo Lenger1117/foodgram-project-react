618526bda4beeb135ad3dd7250d5ad48bbe005bd
Стас Басов <stas.basov@whatever.com>
2023-09-25 20:49:18 +0300

    NewFile5

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -192,3 +192,42 @@ class FavoriteSerializer(serializers.ModelSerializer):
 class ShoppingCartSerializer(FavoriteSerializer):
     class Meta(FavoriteSerializer.Meta):
         model = ShoppingCart
+
+
+class FollowSerializer(UserSerializer):
+    recipes_count = SerializerMethodField()
+    recipes = SerializerMethodField()
+
+    class Meta(UserSerializer.Meta):
+        fields = fields = UserSerializer.Meta.fields + \
+            ('recipes_count', 'recipes')
+        read_only_fields = ['email', 'username', 'first_name', 'last_name']
+
+    def validate(self, data):
+        author_id = self.context.get(
+            'request').parser_context.get('kwargs').get('id')
+        author = get_object_or_404(CustomUser, id=author_id)
+        user = self.context.get('request').user
+        if user.follower.filter(author=author_id).exists():
+            raise ValidationError(
+                detail='Вы уже подписаны на данного пользователя',
+                code=status.HTTP_400_BAD_REQUEST,
+            )
+        if user == author:
+            raise ValidationError(
+                detail='Нельзя подписаться на самого себя',
+                code=status.HTTP_400_BAD_REQUEST,
+            )
+        return data
+
+    def get_recipes_count(self, obj):
+        return obj.recipes.count()
+
+    def get_recipes(self, obj):
+        request = self.context.get('request')
+        limit = request.GET.get('recipes_limit')
+        recipes = obj.recipes.all()
+        if limit:
+            recipes = recipes[: int(limit)]
+        serializer = ShowFavoriteSerializer(recipes, many=True, read_only=True)
+        return serializer.data
