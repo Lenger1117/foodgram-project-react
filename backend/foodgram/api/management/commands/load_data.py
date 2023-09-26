e30574e674af3be035e5f148f58229f09323fac9
Стас Басов <stas.basov@whatever.com>
2023-09-26 21:01:28 +0300

    NewFile10

--- backend/foodgram/api/management/commands/load_data.py
+++ backend/foodgram/api/management/commands/load_data.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         try:
             with open('data/ingredients.json', encoding='utf-8',
-                     ) as data_file_ingredients:
+                      ) as data_file_ingredients:
                 ingredient_data = json.loads(data_file_ingredients.read())
                 for ingredients in ingredient_data:
                     Ingredient.objects.get_or_create(**ingredients)
