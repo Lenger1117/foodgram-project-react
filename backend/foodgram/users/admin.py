c7283187da8877094848a6f85afdc9260c4d6956
Стас Басов <stas.basov@whatever.com>
2023-09-28 14:15:59 +0300

    NewFile7

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -4,7 +4,8 @@ from .models import CustomUser
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'favorites', 'shopping_cart')
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'favorites', 'shopping_cart')
     search_fields = ('username', 'email')
     list_filter = ('first_name', 'last_name')
     ordering = ('username', )
