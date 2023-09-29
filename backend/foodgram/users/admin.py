bb1d1602fd8f3f90609ba018c9dad50f42e577d6
Стас Басов <stas.basov@whatever.com>
2023-09-29 17:32:27 +0300

    NewFile14

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -6,11 +6,14 @@ from .models import CustomUser
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name',
-                    'recipes__favorite')
+                    'recipes_favorite')
     search_fields = ('username', 'email')
     list_filter = ('first_name', 'last_name')
     ordering = ('username', )
     empty_value_display = '-пусто-'
+
+    def recipes_favorite(self, obj):
+        return obj.favorite.recipe
 
 
 admin.site.register(CustomUser, UserAdmin)
