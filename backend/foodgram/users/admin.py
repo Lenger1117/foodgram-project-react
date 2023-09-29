099587e93ce1a4ef92634c5f3c7d9e30a372f94b
Стас Басов <stas.basov@whatever.com>
2023-09-29 15:06:22 +0300

    NewFile12

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -11,6 +11,12 @@ class UserAdmin(admin.ModelAdmin):
     ordering = ('username', )
     empty_value_display = '-пусто-'
 
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
 
 admin.site.register(CustomUser, UserAdmin)
 admin.site.unregister(Group)
