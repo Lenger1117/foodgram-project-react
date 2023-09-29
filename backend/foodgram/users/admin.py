c6440dda2f38c925a8009160777ceac33db9a32a
Стас Басов <stas.basov@whatever.com>
2023-09-29 17:27:00 +0300

    NewFile13

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -5,17 +5,12 @@ from .models import CustomUser
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name')
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'recipes__favorite')
     search_fields = ('username', 'email')
     list_filter = ('first_name', 'last_name')
     ordering = ('username', )
     empty_value_display = '-пусто-'
-
-    def get_model_perms(self, request):
-        """
-        Return empty perms dict thus hiding the model from admin index.
-        """
-        return {}
 
 
 admin.site.register(CustomUser, UserAdmin)
