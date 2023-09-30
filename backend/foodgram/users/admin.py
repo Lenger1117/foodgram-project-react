f03aba3a3b5a023e7fecfd73857f9922da4d5695
Стас Басов <stas.basov@whatever.com>
2023-09-30 20:01:34 +0300

    NewFile8

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from .models import CustomUser
-from django.contrib.admin.utils import quote
-from django.contrib.admin.views.main import ChangeList
-from django.contrib.auth import get_user_model
-from django.core.exceptions import ValidationError
-from django.urls import reverse
-
-from rest_framework.authtoken.models import Token
-
-User = get_user_model()
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -20,38 +11,8 @@ class UserAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
-class TokenChangeList(ChangeList):
-    def url_for_result(self, result):
-        pk = result.user.pk
-        return reverse('admin:%s_%s_change' % (self.opts.app_label,
-                                               self.opts.model_name),
-                       args=(quote(pk),),
-                       current_app=self.model_admin.admin_site.name)
-
-
 class TokenAdmin(admin.ModelAdmin):
-    list_display = ('user', 'created')
-    fields = ('user',)
-    ordering = ('-created',)
-    actions = None
-
-    def get_changelist(self, request, **kwargs):
-        return TokenChangeList
-
-    def get_object(self, request, object_id, from_field=None):
-        queryset = self.get_queryset(request)
-        field = User._meta.pk
-        try:
-            object_id = field.to_python(object_id)
-            user = User.objects.get(**{field.name: object_id})
-            return queryset.get(user=user)
-        except (queryset.model.DoesNotExist, User.DoesNotExist,
-                ValidationError, ValueError):
-            return None
-
-    def delete_model(self, request, obj):
-        token = Token.objects.get(key=obj.key)
-        return super().delete_model(request, token)
+      exclude = ('key',)
 
 
 admin.site.register(CustomUser, UserAdmin)
