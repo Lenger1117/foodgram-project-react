d3a88663053c0594dbb830fa7dd507687a68cce8
Стас Басов <stas.basov@whatever.com>
2023-10-01 18:04:13 +0300

    NewFile17

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from rest_framework.authtoken.models import TokenProxy
 from .models import CustomUser
+from recipes.models import Favorite
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', )
+    list_display = ('username', 'email', 'first_name', 'last_name',
+                    'get_favorite_count', )
     search_fields = ('username', 'email')
     list_filter = ('first_name', 'last_name')
     ordering = ('username', )
     empty_value_display = '-пусто-'
+
+    @admin.display(description='В избранном')
+    def get_favorite_count(self, obj):
+        return Favorite.objects.filter(recipe=obj).count()
 
 
 admin.site.register(CustomUser, UserAdmin)
